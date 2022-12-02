06fff39cdf0181ebd514d84ad1f1776c900f87af
Yielding <90570076+YieldingExploiter@users.noreply.github.com>
2022-12-02 22:20:06 +0100

    feat: add beerware-ify

---
+++ .github/add-license.js
@@ -0,0 +1,71 @@
+/*!
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <pleasego@nuke.africa> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return | YieldingExploiter
+ */
+// config
+const author = 'YieldingExploiter'
+const email = 'pleasego@nuke.africa'
+/* mappings for beerware license */
+const beerWare = {
+  ts: `/*!
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <${email}> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return | ${author}
+ */`,
+  html: `<!--
+ "THE BEER-WARE LICENSE" (Revision 42):
+ <${email}> wrote this file. As long as you retain this notice you
+ can do whatever you want with this stuff. If we meet some day, and you think
+ this stuff is worth it, you can buy me a beer in return | ${author}
+-->`,
+  yaml: `# "THE BEER-WARE LICENSE" (Revision 42):
+# <${email}> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return | ${author}`,
+  lua: `--[[
+  "THE BEER-WARE LICENSE" (Revision 42):
+  <${email}> wrote this file. As long as you retain this notice you
+  can do whatever you want with this stuff. If we meet some day, and you think
+  this stuff is worth it, you can buy me a beer in return | ${author}
+--]]`,
+}
+// aliases
+beerWare.js=beerWare.ts;
+beerWare.css=beerWare.ts;
+beerWare.scss=beerWare.ts;
+beerWare.sass=beerWare.ts;
+beerWare.yml=beerWare.yaml;
+beerWare.env=beerWare.yaml;
+beerWare.gitignore=beerWare.yaml;
+beerWare.htm=beerWare.html;
+beerWare.xml=beerWare.html;
+beerWare.xaml=beerWare.html;
+beerWare.md=beerWare.html+'\n\n';
+// recursive readdirsync - https://coderrocketfuel.com/article/recursively-list-all-the-files-in-a-directory-using-node-js
+const fs = require("fs")
+const path = require("path")
+const recursiveReadDirSync = function(dirPath, arrayOfFiles) {
+  files = fs.readdirSync(dirPath)
+
+  arrayOfFiles = arrayOfFiles || []
+
+  files.forEach(function(file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+    } else {
+      arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
+    }
+  })
+
+  return arrayOfFiles
+}
+// perform shit
+recursiveReadDirSync(process.cwd()).forEach(v=>{
+  const ext = v.split('.').pop().toLowerCase
+  let file = fs.readFileSync(v,'utf-8')
+  if (beerWare[ext] && !file.trim().startsWith(beerWare[ext])) file = `${beerWare[ext]}
+${file}`
+})
