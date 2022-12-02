6fd8dfa7d907c771867acbd4a417dfa4ed286013
Yielding <90570076+YieldingExploiter@users.noreply.github.com>
2022-12-02 21:21:31 +0100

    fix: ignore all combos

--- .github/import-list.js
+++ .github/import-list.js
@@ -3,6 +3,7 @@ let README = fs.readFileSync('index.md','utf-8');
 let importMd = '';
 
 fs.readdirSync('.').forEach((v,idx)=>{
+  if (v.includes('all') && v!=='all.css') return;
   importMd+=`<a href="https://ministyles.astolfo.gay/${v}"><kbd>
   <br/>
   &nbsp;&nbsp;${v}&nbsp;&nbsp;
