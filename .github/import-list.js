4b4ee1defdf89ad278dd1072b427606af50e7c07
Yielding <90570076+YieldingExploiter@users.noreply.github.com>
2022-12-02 21:20:30 +0100

    feat: Add import list script thing

---
+++ .github/import-list.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+let README = fs.readFileSync('index.md','utf-8');
+let importMd = '';
+
+fs.readdirSync('.').forEach((v,idx)=>{
+  importMd+=`<a href="https://ministyles.astolfo.gay/${v}"><kbd>
+  <br/>
+  &nbsp;&nbsp;${v}&nbsp;&nbsp;
+  <br/>
+  <br/>
+</kbd></a>
+`;
+})
+
+README = README.split('%IMPORTLIST%').join(importMd)
+
+fs.writeFileSync('index.md',README)
