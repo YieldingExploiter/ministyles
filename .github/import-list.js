10e631d1f4c45e31e79d1999186a97107c34450e
Yielding <90570076+YieldingExploiter@users.noreply.github.com>
2022-12-02 21:41:10 +0100

    feat: make more unique

--- .github/import-list.js
+++ .github/import-list.js
@@ -6,10 +6,15 @@ let itemCount = 1;
 
 const getCount = (str)=>(str.match(/\+/gi) ?? []).length
 
+let alrHas = [];
+
 fs.readdirSync('.').filter(v=>v.endsWith('.css')).sort((a,b)=>getCount(a)>getCount(b)?1:getCount(a)<getCount(b)?-1:0).forEach((v,idx)=>{
   if (v.includes('all') && v!=='all.css') return;
+  const generic = v.replace('.css','').split('+').sort((a,b)=>a>b?1:a<b?-1:0).join('+');
+  if (alrHas[generic]) return;
+  alrHas.push(generic)
   const thisItemCount = getCount(v);
-  importMd+=`${thisItemCount>itemCount?'<br/>\n':''}<a href="https://ministyles.astolfo.gay/${v}"><kbd>
+  importMd+=`${thisItemCount>itemCount?`\n<br/><br/>\n\n# Combination Depth ${thisItemCount}\n`:''}<a href="https://ministyles.astolfo.gay/${v}"><kbd>
   <br/>
   &nbsp;&nbsp;${v}&nbsp;&nbsp;
   <br/>
