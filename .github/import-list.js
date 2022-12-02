a5c135144e1188bb3c84117e5066e53a022faef4
Yielding <90570076+YieldingExploiter@users.noreply.github.com>
2022-12-02 21:36:53 +0100

    fix: Filter by count

--- .github/import-list.js
+++ .github/import-list.js
@@ -2,15 +2,21 @@ const fs = require('fs')
 let README = fs.readFileSync('index.md','utf-8');
 let importMd = '';
 
-fs.readdirSync('.').forEach((v,idx)=>{
+let itemCount = 1;
+
+const getCount = (str)=>(str.match(/\+/gi) ?? []).length
+
+fs.readdirSync('.').filter(v=>v.endsWith('.css')).sort((a,b)=>getCount(a)>getCount(b)?1:getCount(a)<getCount(b)?-1:0).forEach((v,idx)=>{
   if (v.includes('all') && v!=='all.css') return;
-  importMd+=`<a href="https://ministyles.astolfo.gay/${v}"><kbd>
+  const thisItemCount = getCount(v);
+  importMd+=`${thisItemCount>itemCount?'<br/>\n':''}<a href="https://ministyles.astolfo.gay/${v}"><kbd>
   <br/>
   &nbsp;&nbsp;${v}&nbsp;&nbsp;
   <br/>
   <br/>
 </kbd></a>
 `;
+  itemCount = thisItemCount
 })
 
 README = README.split('%IMPORTLIST%').join(importMd)
