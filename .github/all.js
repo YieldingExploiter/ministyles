3115abee3af52e579f32d024e2693ee66b9afa41
Mokiy <92001109+MokiyCodes@users.noreply.github.com>
2022-12-08 03:22:24 +0100

    fix: Fix critical flaw(s)

--- .github/all.js
+++ .github/all.js
@@ -1,10 +1,12 @@
 // Import
 const fs = require('fs');
 // Define Var
-let all = '';
+let all = '/* Combination of all files */';
 // Concat all files
-fs.readdirSync(process.cwd()).forEach(v=>{
-  all=fs.readFileSync(v,'utf-8');
+fs.readdirSync(process.cwd()).filter(v=>v.toLowerCase().endsWith('.css')).forEach(v=>{
+  all=`${all}
+/* ${v} */
+${fs.readFileSync(v,'utf-8')}`;
 });
 // move @imports to the top
 const lineSplit = all.split('\n').map(v=>v.trimStart()).filter(v=>!v.startsWith('@charset'))
